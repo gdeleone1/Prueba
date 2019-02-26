45773531d4ed54d26bad1eed5329d4cd68f1ac4d
gerson <gerson@gerson-HP-15-Notebook-PC>
2019-02-25 23:21:08 -0600

    Solucion de funcion para balancear parentesis utilizando una estructura stack/

--- ParentesisSolucion1/src/parentesissolucion1/ParentesisSolucion1.java
+++ ParentesisSolucion1/src/parentesissolucion1/ParentesisSolucion1.java
@@ -8,63 +8,32 @@ import java.util.Stack; // libreria para utilizar pila stack
 
 public class ParentesisSolucion1 {
 
-static char [] abiertos = {'(','{','['}; //arreglos para guardar los simbolos 
-static char [] cerrados = {')','}',']'};
-
-static boolean parAbierto(char c){
-    for (int i =0; i< abiertos.length;i++)
-        if (abiertos[i] == c)
-            return true;
-    return false;                //funcion que valida si existen sibolos abiertos
-            
-}
-
-
-
-static char parCerrado(char c){
-    for (int i = 0; i< abiertos.length; i++)
-        if (abiertos[i] ==c){
-            return cerrados [i];
-        }
-  for (int i =0; i< cerrados.length; i++) 
-      if (cerrados[i]==c){
-          return abiertos[i];
-      }
-  return '0';                             //funcion que retorna el cierre que cada signo o la apertura
-}
-
+    
     public static void main(String[] args) {
-     Scanner entradaDatos = new Scanner(System.in);
-     String expresion = entradaDatos.next();
-     char [] expr = expresion.toCharArray();
-     Stack<Character>  pila = new Stack<>();
-     boolean correct =true;
-     for (int i=0; i < expr.length;i++){
-         if (parAbierto(expr[i]))
-             pila.push(expr[i]);
-         else{
-             if(pila.isEmpty()){
-                 correct = false;
-                 break;
-             }else if (pila.peek() != parCerrado(expr[i])){ // si el elemento de la pila es diferente a su 
-              correct = false;                              //cierre es incorrecta la expresion
-              break;
-         }else
-             pila.pop(); // ve el tope de la pila y borra el elemento
-         }
-     
-     
-     }
-    if(!pila.empty())
-        correct = false; //  si la pila no quedo vacia la expresion es incorrecta
-    
-    if (correct)
-     
-        System.out.println(expresion + "es una expresion correcta");
-       else
-          System.out.println (expresion + "No es una expresion correcta");
-    
-    
+        
+        System.out.println("(a[0]+b[2c[6]]){24+53} passed:" + parensMatch ("(a[0]+b[2c[6]]){24+53} :"));
+        System.out.println("f(e(d)) passed:" + parensMatch ("f(e(d)) :"));
+        System.out.println("((b) passed:" + parensMatch ("((b):"));
+        
     }
     
+      public static boolean parensMatch(String sentence) {
+          
+     Stack<Character> stackSimbolos = new Stack<Character>();
+    char arraySimbolos[] = sentence.toCharArray();
+    int i;
+    for (i = 0; i < arraySimbolos.length; i++) {
+      if (arraySimbolos[i] == '(' || arraySimbolos[i]=='{' || arraySimbolos [i]== '[' ) {
+        stackSimbolos.push(arraySimbolos[i]);
+      }else if (arraySimbolos[i] == ')' || arraySimbolos [i]=='}' || arraySimbolos [i]==']') {
+        if (!stackSimbolos.empty() && stackSimbolos.peek() != ')') {
+          stackSimbolos.pop();
+        }else{
+          stackSimbolos.push(arraySimbolos[i]);
+        }
+      }
+    }
+  return stackSimbolos.empty();
+ 
+}
 }
